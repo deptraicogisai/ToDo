b7cf7b7af84df17fe143f9997cc1ae877848a188
Tran Phuoc Anh Vu <anhvu3890@gmail.com>
2016-12-03 11:00:34 +0700

    First

---
+++ app/home.tsx
@@ -0,0 +1,14 @@
+import  * as React from 'react'
+
+interface HomeState {
+    title: string
+}
+
+export class Home extends React.Component<{},{}> {
+
+    render() {
+        return (
+            <div>Home Page</div>
+        )
+    }
+}
---
+++ dist/public/application/bundle.js
@@ -0,0 +1,88 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/dist/public/application";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	const React = __webpack_require__(2);
+	const ReactDOM = __webpack_require__(3);
+	const home_1 = __webpack_require__(4);
+	ReactDOM.render(React.createElement(home_1.Home, null), document.getElementById('root'));
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports = React;
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = ReactDOM;
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	const React = __webpack_require__(2);
+	class Home extends React.Component {
+	    render() {
+	        return (React.createElement("div", null, "Home Page"));
+	    }
+	}
+	exports.Home = Home;
+
+
+/***/ }
+/******/ ]);
