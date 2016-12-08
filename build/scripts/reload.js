e6df697b2d29fce8fde6c7f17e766362ab68f26c
Tran Phuoc Anh Vu <anhvu3890@gmail.com>
2016-12-08 16:14:43 +0700

    Commit js

---
+++ build/scripts/reload.js
@@ -0,0 +1,25 @@
+var client = new WebSocket("ws://localhost:3001/");
+
+client.onopen = function () {
+    /// ping with server
+    window.setInterval(function () {
+        client.send("ping");
+    }, 1000);
+};
+
+/// reload on message
+client.onmessage = function (event) {
+    console.log(event);
+    if (event.data == "reloading") {
+        $(document.body).append(
+                "<div id=\"reloader\" class=\"ui active inverted dimmer\">" +
+                    "<div class=\"ui text loader\">Loading</div>" +
+                "</div>");
+    } else if (event.data == "reload") {
+        location.reload();
+    } else if (event.data == "reload-css") {
+        $("#reloader").detach();
+        $(document.body).append(
+            "<link rel='stylesheet' href='./styles/main.css?" + Math.random() + "' />");
+    }
+};
